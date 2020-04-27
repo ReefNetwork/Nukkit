0e2135be0fb1b729b5e8be5aa988d4b7681c2327
Ree_ <ree.dev.jp@gmail.com>
2020-04-27 16:02:10 +0900

    update: gitlab-ci config - dockerfileが見つからない問題を修正

--- run.cmd
+++ run.cmd
@@ -1,2 +1,5 @@
-docker build -t coral_reef
+docker build -t coral_reef .
+@echo (Even if you answer N, you can continue)
+@docker image prune
+mkdir data
 docker run -it --rm -p 19132:19132/udp -v /data:/data --name CoralReef  coral_reef
