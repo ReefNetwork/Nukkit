472ce4d833fb130c13bf2ec350fe0f40301d0c8c
Ree_ <ree.dev.jp@gmail.com>
2020-05-08 22:35:45 +0900

    update: 簡単な処理はラムダで出来るように

--- src/main/java/cn/nukkit/form/element/ElementButton.java
+++ src/main/java/cn/nukkit/form/element/ElementButton.java
@@ -3,15 +3,26 @@ package cn.nukkit.form.element;
 public class ElementButton {
 
     private String text = "";
+    public Runnable task = null;
     private ElementButtonImageData image;
 
     public ElementButton(String text) {
         this.text = text;
     }
 
+    public ElementButton(String text, Runnable task) {
+        this.text = text;
+        this.task = task;
+    }
+
     public ElementButton(String text, ElementButtonImageData image) {
         this.text = text;
         if (!image.getData().isEmpty() && !image.getType().isEmpty()) this.image = image;
+    }
+
+    public void runTask() {
+        if (this.task == null) return;
+        this.task.run();
     }
 
     public String getText() {
