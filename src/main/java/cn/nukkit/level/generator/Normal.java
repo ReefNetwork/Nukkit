6470f2e96fd5997a19dbf3c305ef94822bc0738a
Ree_ <ree.dev.jp@gmail.com>
2020-05-14 16:10:34 +0900

    update: 鉱石の確率さげーの

--- src/main/java/cn/nukkit/level/generator/Normal.java
+++ src/main/java/cn/nukkit/level/generator/Normal.java
@@ -190,12 +190,12 @@ public class Normal extends Generator {
 
         PopulatorOre ores = new PopulatorOre();
         ores.setOreTypes(new OreType[]{
-                new OreType(Block.get(BlockID.COAL_ORE), 20, 17, 0, 128),
-                new OreType(Block.get(BlockID.IRON_ORE), 20, 9, 0, 64),
-                new OreType(Block.get(BlockID.REDSTONE_ORE), 8, 8, 0, 16),
-                new OreType(Block.get(BlockID.LAPIS_ORE), 1, 7, 0, 16),
-                new OreType(Block.get(BlockID.GOLD_ORE), 2, 9, 0, 32),
-                new OreType(Block.get(BlockID.DIAMOND_ORE), 1, 8, 0, 16),
+                new OreType(Block.get(BlockID.COAL_ORE), 8, 8, 0, 128),
+                new OreType(Block.get(BlockID.IRON_ORE), 5, 5, 0, 64),
+                new OreType(Block.get(BlockID.REDSTONE_ORE), 3, 3, 0, 16),
+                new OreType(Block.get(BlockID.LAPIS_ORE), 1, 4, 0, 16),
+                new OreType(Block.get(BlockID.GOLD_ORE), 2, 3, 0, 32),
+                new OreType(Block.get(BlockID.DIAMOND_ORE), 1, 1, 0, 13),
                 new OreType(Block.get(BlockID.DIRT), 10, 33, 0, 128),
                 new OreType(Block.get(BlockID.GRAVEL), 8, 33, 0, 128),
                 new OreType(Block.get(BlockID.STONE, BlockStone.GRANITE), 10, 33, 0, 80),
